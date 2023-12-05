2f421d4f973064064a3488570e272e84c566a79d
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2023-12-05 09:39:53 -0300

    atualização

--- pages/receita4/pages/movies.js
+++ pages/receita4/pages/movies.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import styles from "../styles/movies.module.css";
+import styles from "./styles/movies.module.css";
 
 export default function Movies({ startData }) {
   const [searchQuery, setSearchQuery] = useState("");
